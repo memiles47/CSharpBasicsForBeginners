e97dbb759a2db650dd8949f7ace0fd933d1abb9e
Michael Miles <boat081062@gmail.com>
2016-04-13 05:48:58 -0500

    Comment about ReSharper

--- Lecture22-Operators/Lecture22-Operators/Program.cs
+++ Lecture22-Operators/Lecture22-Operators/Program.cs
@@ -4,11 +4,13 @@ namespace Lecture22_Operators
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var a = 10;
             var b = 3;
 
+            //ReSharper removed the first case of float from 'a' as not needed
+            //and the result compiled and was the same as casting both a and b
             Console.WriteLine(a / (float)b);
         }
     }
