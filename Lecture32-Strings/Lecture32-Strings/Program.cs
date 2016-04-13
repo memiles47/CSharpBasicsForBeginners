a68bb405521b05313d5468980698ccd21d7f608b
Michael Miles <boat081062@gmail.com>
2016-04-13 15:39:01 -0500

    String formatting methods

---
+++ Lecture32-Strings/Lecture32-Strings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lecture32_Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            var firstName = "Michael";
+            var lastName = "Miles";
+
+            string fullName = $"My name is {firstName} {lastName}";
+
+            var names = new string[3] {"John", "Jack", "Mary"};
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+            var text =
+                "Hi John\nLook into the following paths\n\nc:\\folder1\\folder2\nc:\\folder1\\folder2\\folder3\\folder4";
+            Console.WriteLine(text);
+
+            var text2 = @"Hi John
+Look into the following paths
+
+c:\folder1\folder2
+c:\folder1\folder2\folder3\folder4";
+            Console.WriteLine(text2);
+        }
+    }
+}
