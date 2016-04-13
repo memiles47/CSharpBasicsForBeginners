e7726f214241a940ad9e47fc591850436e0c1fa3
Michael Miles <boat081062@gmail.com>
2016-04-13 07:17:01 -0500

    This course is quite good for explaining the basics of CSharp. Using ReSharper at the same time really re-enforces concepts I have learned in other course and books. He should get commissions from the ReSharper team for increased sales.

---
+++ Lecture26-DeclaringClasses/Lecture26-DeclaringClasses/Math/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Lecture26_DeclaringClasses.Math
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Lecture26-DeclaringClasses/Lecture26-DeclaringClasses/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Lecture26_DeclaringClasses.Math;
+
+namespace Lecture26_DeclaringClasses
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var michael = new Person
+            {
+                FirstName = "Michael",
+                LastName = "Miles"
+            };
+            michael.Introduce();
+
+            var calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine($"a + b = {result}");
+        }
+    }
+}
