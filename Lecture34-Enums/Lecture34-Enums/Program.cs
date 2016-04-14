399112df8773d8dc8db6f1eb94d6e307d7eee835
Michael Miles <boat081062@gmail.com>
2016-04-14 06:03:51 -0500

    Adding examples of reference and value type objects and how they are treated in memory

---
+++ Lecture34-Enums/Lecture34-Enums/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lecture34_Enums
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodID = 3;
+            Console.WriteLine((ShippingMethod)methodID);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var  shippinpMethod = (ShippingMethod)Enum.Parse(typeof (ShippingMethod), methodName);
+            Console.WriteLine(shippinpMethod);
+
+        }
+    }
+}
