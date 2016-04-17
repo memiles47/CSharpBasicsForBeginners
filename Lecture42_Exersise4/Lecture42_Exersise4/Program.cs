c69e626a48ac2eb421040a69ae9eabfb94958cff
Michael E Miles <michael@miles-me.net>
2016-04-17 09:48:48 -0500

    Exersise 4

---
+++ Lecture42_Exersise4/Lecture42_Exersise4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lecture42_Exersise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Enter the Speed Limit: ");
+            var stringSpeedLimit = Console.ReadLine();
+
+            Console.Write($"Enter the Speed of the vehicle: ");
+            var stringVehicleSpeed = Console.ReadLine();
+
+            var speedLimit = int.Parse(stringSpeedLimit);
+            var vehicleSpeed = int.Parse(stringVehicleSpeed);
+
+            var demerits = (vehicleSpeed - speedLimit) / 5;
+
+            if (vehicleSpeed <= speedLimit)
+            {
+                Console.WriteLine($"Ok");
+            }
+            else if (demerits < 12)
+            {
+                Console.WriteLine($"\nDemerits = {demerits}");
+            }
+            else
+            {
+                Console.WriteLine($"\nLicense suspended");
+            }
+        }
+    }
+}
