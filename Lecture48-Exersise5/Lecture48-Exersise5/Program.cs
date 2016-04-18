f2b338d0041264e23459f809d9e49a0cba31e9ad
Michael Miles <boat081062@gmail.com>
2016-04-18 05:16:59 -0500

    Commented my code for exercise 5.

--- Lecture48-Exersise5/Lecture48-Exersise5/Program.cs
+++ Lecture48-Exersise5/Lecture48-Exersise5/Program.cs
@@ -6,11 +6,15 @@ namespace Lecture48_Exersise5
     {
         static void Main()
         {
+            //This is how I envisioned creating this string.
             Console.Write($"Enter a series of numbers separated by a comma: ");
             var userInput = Console.ReadLine() ?? "";
 
+            //This is what I was thinking about in bed and did not know how to accomplish this
             var numbers = userInput.Split(',');
 
+            //Between this and the foreach construct is how I planned to do this (brute force)
+            //or use the .Max command if I had converted the entire array into integers
             var max = Convert.ToInt32(numbers[0]);
 
             foreach (var item in numbers)
