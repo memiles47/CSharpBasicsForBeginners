89431d7f269d97e9cbd93a731d405aff0a273ccb
Michael Miles <boat081062@gmail.com>
2016-04-18 15:35:23 -0500

    Lecture55, Exercies 1 -

---
+++ Lecture54_ListFunctions/Lecture54_ListFunctions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lecture54_ListFunctions
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = new List<int> {1, 2, 3, 4, 1};
+            numbers.AddRange(new[] {5,6,7});
+
+            foreach(var n in numbers)
+                Console.WriteLine($"{n}");
+
+            //IndexOf()
+            Console.WriteLine();
+            Console.WriteLine($"Index of 1: {numbers.IndexOf(1)}");
+
+            //LastIndexOf()
+            Console.WriteLine($"Last Index of 1: {numbers.LastIndexOf(1)}");
+
+            //Count()
+            Console.WriteLine($"Number if items in list: {numbers.Count}");
+
+            //Remove()
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                    numbers.Remove(numbers[i]);
+            }
+            foreach(var n in numbers)
+                Console.WriteLine($"{n}");
+        }
+    }
+}
---
+++ Lecture55_Exercise1/Lecture55_Exercise1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lecture55_Exercise1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var friends = new List<string>();
+
+            while (true)
+            {
+                Console.Write($"Friends name: ");
+                var fbFriend = Console.ReadLine();
+
+                friends.Add(fbFriend);
+
+                switch (friends.Count)
+                {
+                    case 1:
+                        Console.WriteLine($"Friend One: {friends[0]}");
+                        break;
+
+                    case 2:
+                        Console.WriteLine($"A Second Friend: {friends[1]}");
+                        break;
+
+                    default:
+                        Console.WriteLine($"A New Friend: {friends[friends.Count - 1]}");
+                        break;
+                }
+                foreach(var name in friends)
+                    Console.WriteLine(name);
+            }
+        }
+    }
+}
