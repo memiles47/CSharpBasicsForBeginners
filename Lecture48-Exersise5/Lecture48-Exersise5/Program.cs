bc030989636df32fc7f5afb518849ae8ebf72c00
Michael E Miles <michael@miles-me.net>
2016-04-18 03:22:35 -0500

    Working on final exersise 5

--- Lecture48-Exersise5/Lecture48-Exersise5/Program.cs
+++ Lecture48-Exersise5/Lecture48-Exersise5/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace Lecture48_Exersise5
+﻿using System;
+
+namespace Lecture48_Exersise5
 {
     class Program
     {
         static void Main()
         {
+            Console.Write($"Enter a series of numbers separated by a comma: ");
+            var userInput = Console.ReadLine();
+
+            var numbers = userInput.Split(',');
+
+            var max = Convert.ToInt32(numbers[0]);
+
+            Console.WriteLine($"{numbers.Length}");
         }
     }
 }
