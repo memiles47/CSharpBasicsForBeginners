a114620dc550233224f19637d53ca538a8d9601f
Michael Miles <boat081062@gmail.com>
2016-04-19 08:22:14 -0500

    Lesson for today. Look at all the methods and properties of a class or struct or enum....

--- Lecture55_Exercise3/Lecture55_Exercise3/Program.cs
+++ Lecture55_Exercise3/Lecture55_Exercise3/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Lecture55_Exercise3
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lecture55_Exercise3
 {
     class Program
     {
         static void Main()
         {
+            var numbers = new List<int>();
 
+            while (numbers.Count < 5)
+            {
+                Console.Write($"Number {numbers.Count + 1}: ");
+                var newNumber = Convert.ToInt32(Console.ReadLine());
+                if (numbers.Contains(newNumber))
+                {
+                    Console.WriteLine($"Number {newNumber} already exists");
+                    continue;
+                }
+                numbers.Add(newNumber);
+            }
+
+            numbers.Sort();
+            foreach (var n in numbers)
+                Console.WriteLine($"Number {n}");
         }
     }
 }
