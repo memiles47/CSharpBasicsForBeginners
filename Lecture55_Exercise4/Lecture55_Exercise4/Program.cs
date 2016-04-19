4decd37d76c0092573ddc6f8d2a42659259674bc
Michael Miles <boat081062@gmail.com>
2016-04-19 11:23:38 -0500

    My solution is more accurate to the exercise question than his.

---
+++ Lecture55_Exercise4/Lecture55_Exercise4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lecture55_Exercise4
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = new List<int>();
+            var unique = new List<int>();
+
+            while (true)
+            {
+                Console.Write($"Enter a number or quit to exit: ");
+                var input = Console.ReadLine();
+
+                if (input?.ToLower() == "quit") break;
+
+                numbers.Add(Convert.ToInt32(input));
+                if(!unique.Contains(Convert.ToInt32(input)))
+                    unique.Add(Convert.ToInt32(input));
+            }
+            foreach(var n in unique)
+                Console.WriteLine(n);
+        }
+    }
+}
---
+++ Lecture55_Exercise5/Lecture55_Exercise5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lecture55_Exercise5
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbersList;
+
+            while (true)
+            {
+                Console.WriteLine($"Please enter a list of comma separated values (e.g. 1, 2, 3, ...)");
+                var input = Console.ReadLine() ?? "";
+                var cleanInput = input.Replace(" ", "");
+
+                if (!string.IsNullOrWhiteSpace(cleanInput)) //Remember string.IsNullOrWhiteSpace
+                {
+                    var numbers = cleanInput.Split(',');
+                    if (numbers.Length > 4)
+                    {
+                        numbersList = numbers.Select(n => Convert.ToInt32(n)).ToList(); //Remember this...
+                        break;
+                    }
+                }
+
+                Console.WriteLine($"Invalid entry retry");
+                Console.Write($"Press <ENTER> to continue... ");
+                Console.ReadLine();
+                Console.Clear();
+            }
+
+            numbersList.Sort();
+            Console.WriteLine($"The three smallest numbers are: ");
+            for (var i = 0; i < 3; i++)
+                Console.WriteLine($"Number {i + 1}: {numbersList[i]}");
+        }
+    }
+}
