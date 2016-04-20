bc308f9f71955770afeeac7c2ae0334059fc70a5
Michael E Miles <michael@miles-me.net>
2016-04-20 17:54:31 -0500

    starting exercise 3

--- Lecture67_Exercise3/Lecture67_Exercise3/Program.cs
+++ Lecture67_Exercise3/Lecture67_Exercise3/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Lecture67_Exercise3
+﻿using System;
+
+namespace Lecture67_Exercise3
 {
     class Program
     {
         static void Main()
         {
-
+            Console.Write("");
         }
     }
 }
