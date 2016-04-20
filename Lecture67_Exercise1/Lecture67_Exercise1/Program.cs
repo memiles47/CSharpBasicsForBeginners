fff3cb5a6442976492a68dd15cd70e49526faff6
Michael E Miles <michael@miles-me.net>
2016-04-20 03:37:12 -0500

    Try the algorithim again.

--- Lecture67_Exercise1/Lecture67_Exercise1/Program.cs
+++ Lecture67_Exercise1/Lecture67_Exercise1/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main()
         {
-
+            var numbers1 = new int[] {1, 2, 3, 4};
+            var numbers2 = new int[] {2, 5, 1, 7};
+            var numbers3 = new int[] {4, 3, 2, 1};
         }
     }
 }
