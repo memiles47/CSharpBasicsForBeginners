356c3c775c2b1d6268427a000546c26ead1c759f
Michael Miles <boat081062@gmail.com>
2016-04-20 13:05:18 -0500

    Experiments are working

--- Lecture67_Exercies4/Lecture67_Exercies4/Program.cs
+++ Lecture67_Exercies4/Lecture67_Exercies4/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace Lecture67_Exercies4
+﻿using System;
+using System.Text;
+
+namespace Lecture67_Exercies4
 {
     class Program
     {
-        static void Main(s)
+        static void Main()
         {
+            //var words = "my nEw vaRiaBle";
+            //var varible = words.Split(' ');
+            var testWord = "miCHaEl";
+
+
+            var bldrVariableName = new StringBuilder();
+
+            bldrVariableName.Append((testWord[0].ToString().ToUpper()))
+                .Append(testWord.Substring(1).ToLower());
+
+            Console.WriteLine($"New word: {bldrVariableName}");
+
 
         }
     }
