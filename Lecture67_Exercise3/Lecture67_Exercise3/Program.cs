54b6b163447afa37b858f4a2b433a7cbc425e56c
Michael E Miles <michael@miles-me.net>
2016-04-25 10:20:42 -0500

    Exercise 3 is complete however I don't think is the best solution

--- Lecture67_Exercise3/Lecture67_Exercise3/Program.cs
+++ Lecture67_Exercise3/Lecture67_Exercise3/Program.cs
@@ -6,11 +6,31 @@ namespace Lecture67_Exercise3
     {
         static void Main()
         {
-            Console.Write("Please enter a time in 12hr format e.g. 19:50");
+            Console.Write("Please enter a time in 12hr format e.g. 19:50> ");
             var timeInput = Console.ReadLine() ?? "" ;
 
             var timeArray = timeInput.Split(':');
-            var dateTime = new DateTime();
+
+            Console.WriteLine($"The entered time: {timeInput} is {TimeCheck(timeArray)}.");
+        }
+
+        static string TimeCheck(string[] inputTime)
+        {
+            if (inputTime[0] == "")
+            {
+                return "Invalid";
+            }
+            if (Convert.ToInt32(inputTime[0]) < 0 || Convert.ToInt32(inputTime[0]) > 23)
+            {
+                return "Invalid";
+            }
+
+            if (Convert.ToInt32(inputTime[1]) < 0 || Convert.ToInt32(inputTime[1]) > 59)
+            {
+                return "Invalid";
+            }
+
+            return "Valid";
         }
     }
 }
