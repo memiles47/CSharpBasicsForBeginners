4166fb552e2e2982ab01428c4bb1ae217c9915c4
Michael E Miles <michael@miles-me.net>
2016-04-25 14:45:29 -0500

    Completed all exercises in Lecture 75 regarding file, fileinfo, directory, directoryinfo and path - easy.

---
+++ Lecture75_Exercise1/Lecture75_Exercise1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Lecture75_Exercise1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var path = @"C:/Users/Michael/Data/TextDocument.txt";
+
+            Console.WriteLine($"The number of words in the text file is: {NumberOfWords(path)}");
+        }
+
+        static int NumberOfWords(string path)
+        {
+            var wordString = File.ReadAllText(path);
+            var words = wordString.Split(' ');
+            return words.Length;
+        }
+    }
+}
---
+++ Lecture75_Exercise2/Lecture75_Exercise2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Lecture75_Exercise2
+{
+    class Program
+    {
+        static void Main()
+        {
+            var path = @"C:/Users/Michael/Data/TextDocument.txt";
+            Console.WriteLine($"The maximum word length in the text file is: {MaxWordLength(path)}");
+        }
+
+        static int MaxWordLength(string path)
+        {
+            var wordString = File.ReadAllText(path);
+            var cleanString = wordString.Replace(".", "");
+            var words = cleanString.Split(' ');
+            var max = words[0].Length;
+
+            foreach (var word in words)
+            {
+                if (word.Length > max) max = word.Length;
+            }
+            return max;
+        }
+
+    }
+}
