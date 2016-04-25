338efed89564223444bbfa41eb2386f458677343
Michael E Miles <michael@miles-me.net>
2016-04-25 11:06:59 -0500

    Start Exercise 2

--- Lecture67_Exercise2/Lecture67_Exercise2/Program.cs
+++ Lecture67_Exercise2/Lecture67_Exercise2/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace Lecture67_Exercise2
+﻿using System;
+
+namespace Lecture67_Exercise2
 {
     class Program
     {
         static void Main()
         {
+            Console.Write("Please enter a series of numbers separated with a hyphen e.g. 1-2-3...: ");
+            var input = Console.ReadLine() ?? "";
+            if (input == "")
+                return;
+
+
 
         }
     }
