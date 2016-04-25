551fcc719656ffd872a85ad9d1de27fe3b61d19a
Michael E Miles <michael@miles-me.net>
2016-04-25 11:58:10 -0500

    Exercise complete

--- Lecture67_Exercise2/Lecture67_Exercise2/Program.cs
+++ Lecture67_Exercise2/Lecture67_Exercise2/Program.cs
@@ -6,13 +6,30 @@ namespace Lecture67_Exercise2
     {
         static void Main()
         {
+            var duplicates = false;
+
             Console.Write("Please enter a series of numbers separated with a hyphen e.g. 1-2-3...: ");
             var input = Console.ReadLine() ?? "";
             if (input == "")
                 return;
 
+            var inputArray = input.Split('-');
 
+            foreach (var n in inputArray)
+            {
+                var count = 0;
 
+                for (int i = 0; i < inputArray.Length - 1; i++)
+                {
+                    if (n == inputArray[i])
+                    {
+                        count ++;
+                        if (count > 1) duplicates = true;
+                    }
+                }
+            }
+
+            Console.WriteLine($"List contains duplicates: {(duplicates ? "Yes": "No")}");
         }
     }
 }
