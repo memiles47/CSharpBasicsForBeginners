348e350a9ae71a047e23070fdd632c477ef70f98
Michael E Miles <michael@miles-me.net>
2017-03-19 14:52:20 -0500

    Continuation

--- Lecture26-DeclaringClasses/Lecture26-DeclaringClasses/Program.cs
+++ Lecture26-DeclaringClasses/Lecture26-DeclaringClasses/Program.cs
@@ -3,7 +3,7 @@ using Lecture26_DeclaringClasses.Math;
 
 namespace Lecture26_DeclaringClasses
 {
-    //Checking my memory of this small program
+    //This next six months is going to drive me insane.
     internal class Program
     {
         private static void Main()
